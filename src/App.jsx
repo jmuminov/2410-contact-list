58f6397e0202e32cc139270992a83514a2c18cc4
Jamoliddin Muminov <jmuminov92@gmail.com>
2025-01-15 19:54:57 -0500

    Addeded contactList.jsx, contactRow.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import ContactList from './components/ContactList'
 
-function App() {
-
+export default function App() {
   return (
     <>
-      
+      <ContactList />
     </>
-  )
+  );
 }
-
-export default App
